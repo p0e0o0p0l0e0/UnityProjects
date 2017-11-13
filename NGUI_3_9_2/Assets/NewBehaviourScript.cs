c0164cede7281b2c6dbf7a069031602ff43bb035
Linqi Zhang <p0e0o0p0l0e0@sina.com>
2017-11-13 18:15:57 +0800

    NGUI_3_9_2 project

---
+++ NGUI_3_9_2/Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	Stack _stack = new Stack();
+
+	// Use this for initialization
+	void Start () {
+		_stack.Push(1);
+		_stack.Push(2);
+
+		object[] s = _stack.ToArray();
+
+		for(int i = 0; i < s.Length; i++)
+		{
+			Debug.Log(s[i]);
+		}
+	}
+}
