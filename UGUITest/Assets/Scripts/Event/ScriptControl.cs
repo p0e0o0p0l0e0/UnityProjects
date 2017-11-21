fde10b03348bf61792bed6af80c3154b57305eb1
Linqi Zhang <p0e0o0p0l0e0@sina.com>
2017-11-21 17:05:41 +0800

    UGUITest: 使用代码添加EventTrigger给cube添加点击事件。

---
+++ UGUITest/Assets/Scripts/Event/ScriptControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class ScriptControl : MonoBehaviour
+{
+	// Use this for initialization
+	void Start () {
+		EventTrigger trigger = transform.gameObject.GetComponent<EventTrigger>();
+		if (trigger == null)
+			trigger = transform.gameObject.AddComponent<EventTrigger>();
+		//定义所要绑定的事件类型
+		EventTrigger.Entry entry = new EventTrigger.Entry();
+		//设置事件类型
+		entry.eventID = EventTriggerType.PointerClick;
+		//设置回调函数：注意这个callback是从EventTrigger的OnPointerClick里执行的，因此如果此类重写了OnPointerClick则不会执行。
+		entry.callback.AddListener(new UnityAction<BaseEventData>(OnScriptControl));
+		//添加事件触发记录到GameObject的事件触发组件
+		trigger.triggers.Add(entry);
+	}
+
+	public void OnScriptControl(BaseEventData arg0)
+	{
+		Debug.Log("Cube Test Click");
+	}
+}
