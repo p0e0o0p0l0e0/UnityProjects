a8aa187d1cb3f2c6bcd19a81cf7013b2a60e4e01
Linqi Zhang <p0e0o0p0l0e0@sina.com>
2017-11-21 11:30:56 +0800

    UGUITest: add event trigger listener

---
+++ UGUITest/Assets/Scripts/EventTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EventTest : MonoBehaviour
+{
+	Button	button;
+	Image image;
+
+	// Use this for initialization
+	void Start ()
+	{
+		button = transform.Find("Button").GetComponent<Button>();
+		image = transform.Find("Image").GetComponent<Image>();
+		EventTriggerListener.Get(button.gameObject).onClick = OnButtonClick;
+		EventTriggerListener.Get(image.gameObject).onClick = OnButtonClick;
+	}
+
+	private void OnButtonClick (GameObject go)
+	{
+		Debug.Log ("DoSomeThings");
+	}
+}
