6bf184e8ac5bb6870b306cdb832f67e908ec1e9b
Linqi Zhang <p0e0o0p0l0e0@sina.com>
2017-11-22 16:07:03 +0800

    UGUITest: 添加判断是否点击到UI，但方法有问题，无法区分3D物体和UI。

--- UGUITest/Assets/Scripts/GameStart.cs
+++ UGUITest/Assets/Scripts/GameStart.cs
@@ -1,7 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
 
 public class GameStart : MonoBehaviour {
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0)||(Input.touchCount >0 && Input.GetTouch(0).phase == TouchPhase.Began))
+		{
+			#if UNITY_ANDROID || UNITY_IPHONE
+			if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+			#else
+			if (EventSystem.current.IsPointerOverGameObject())
+			#endif
+				Debug.Log("当前触摸在UI上");
+			
+			else 
+				Debug.Log("当前没有触摸在UI上");
+		}
+	}
+
 
 	void OnApplicationQuit ()
 	{
