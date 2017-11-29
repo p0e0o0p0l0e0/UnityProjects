cf5658baa96164170e79ef0865590b5a83630443
张琳琪 <p0e0o0p0l0e0@sina.com>
2017-11-29 18:27:34 +0800

    algorithm test project : 快速排序：找一个pivot，小于它的放前面，其他放后面，递归排序。

--- CSharpProject/algorithmtest/algorithmtest/Program.cs
+++ CSharpProject/algorithmtest/algorithmtest/Program.cs
@@ -22,7 +22,8 @@ namespace algorithmtest
             //MergeSortRecursion(array, 0, array.Length - 1);
             //MergeSortIteration(array, array.Length);
             //HeapSort(array, array.Length);
-            HeapSort(array);
+            //HeapSort(array);
+            QuickSort(array, 0, array.Length - 1);
             ShowResult();
         }
 
@@ -406,9 +407,43 @@ namespace algorithmtest
             }
         }
 
+        // 快速排序(Quick Sort)
+        // 分类 ------------ 内部比较排序
+        // 数据结构 --------- 数组
+        // 最差时间复杂度 ---- 每次选取的基准都是最大（或最小）的元素，导致每次只划分出了一个分区，需要进行n-1次划分才能结束递归，时间复杂度为O(n^2)
+        // 最优时间复杂度 ---- 每次选取的基准都是中位数，这样每次都均匀的划分出两个分区，只需要logn次划分就能结束递归，时间复杂度为O(nlogn)
+        // 平均时间复杂度 ---- O(nlogn)
+        // 所需辅助空间 ------ 主要是递归造成的栈空间的使用(用来保存left和right等局部变量)，取决于递归树的深度，一般为O(logn)，最差为O(n)       
+        // 稳定性 ---------- 不稳定
+        static int Partition (int[] A, int left, int right)
+        {
+            int pivot = A[right];
+            int tail = left - 1;
+
+            for (int i = left; i < right; i++)
+            {
+                if(A[i] <= pivot)       // 将小于pivot的数值放在right左侧
+                {
+                    Swap(A, ++tail, i);
+                }
+            }
+            Swap(A, tail + 1, right);   // 将大于pivot的放在right右侧
+            return tail + 1;
+        }
+
+        static void QuickSort(int[] A, int left, int right)
+        {
+            if (left >= right)
+                return;
+
+            int pivot_index = Partition(A, left, right);
+            QuickSort(A, left, pivot_index - 1);    // 递归排序左侧
+            QuickSort(A, pivot_index + 1, right);   // 递归排序右侧
+        }
+
         static void Swap(int[] A, int i, int j)
         {
-			Console.WriteLine("Swap: A{0} = {2} and A{1} = {3}", i, j, A[i], A[j]);
+			//Console.WriteLine("Swap: A{0} = {2} and A{1} = {3}", i, j, A[i], A[j]);
             int temp = A[i];
             A[i] = A[j];
             A[j] = temp;
