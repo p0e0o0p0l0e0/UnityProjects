10e37defcc599c76aa6a71ef0cbc0cba60a74f6c
张琳琪 <p0e0o0p0l0e0@sina.com>
2017-12-07 11:45:48 +0800

    GameFramework: event example

---
+++ GameFramework/Assets/Example/EventExample.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using GameFramework.Event;
+using UnityGameFramework.Runtime;
+using GameFramework;
+
+namespace UnityGameFramework
+{
+	public class EventExample : MonoBehaviour
+	{
+		void Start()
+		{
+			RunEventExample ();
+		}
+
+		private void RunEventExample()
+		{
+			EventComponent eventComponent = GameEntry.GetComponent<EventComponent>();
+			eventComponent.Subscribe((int)EventId.PlayerHPChanged, OnPlayerHPChanged);
+			eventComponent.Fire(this, new PlayerHPChangedEventArgs(1000));
+		}
+
+		private void OnPlayerHPChanged(object sender, GameEventArgs e)
+		{
+			PlayerHPChangedEventArgs ne = e as PlayerHPChangedEventArgs;
+			Log.Info("New HP is '{0}'.", ne.NewHP.ToString());
+		}
+	}
+}
---
+++ GameFramework/Assets/GameFramework/Scripts/Runtime/Event/PlayerHPChangedEventArgs.cs
@@ -0,0 +1,61 @@
+﻿/// <summary>
+/// 玩家生命值改变事件。
+/// </summary>
+
+using GameFramework.Event;
+
+namespace UnityGameFramework.Runtime
+{
+	/// <summary>
+	/// 事件类型编号。
+	/// </summary>
+	public enum EventId
+	{
+		GameEventStart,
+
+		/// <summary>
+		/// 玩家生命值改变事件。
+		/// </summary>
+		PlayerHPChanged,
+	}
+
+	public class PlayerHPChangedEventArgs : GameEventArgs
+	{
+		/// <summary>
+		/// 初始化玩家生命值改变事件的新实例。
+		/// </summary>
+		/// <param name="newHP">玩家新的生命值。</param>
+		public PlayerHPChangedEventArgs(int newHP)
+		{
+			NewHP = newHP;
+		}
+
+		/// <summary>
+		/// 获取玩家生命值改变事件编号。
+		/// </summary>
+		public override int Id
+		{
+			get
+			{
+				return (int)EventId.PlayerHPChanged;
+			}
+		}
+
+		/// <summary>
+		/// 玩家新的生命值。
+		/// </summary>
+		public int NewHP
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// 清理下载成功事件。
+		/// </summary>
+		public override void Clear()
+		{
+			NewHP = 0;
+		}
+	}
+}
