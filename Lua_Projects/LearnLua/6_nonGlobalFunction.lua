288836deb1de075cfec2da34011bca90abd783be
CYOU-INC\zhanglinqi <p0e0o0p0l0e0@sina.com>
2018-02-17 16:46:09 +0800

    6 将函数存储在table中

--- Lua_Projects/LearnLua/6_nonGlobalFunction.lua
+++ Lua_Projects/LearnLua/6_nonGlobalFunction.lua
@@ -1,5 +1,13 @@
 Lib = {}
 
+-- Lib.foo = function (x, y) return x+y end
+-- Lib.goo = function (x, y) return x-y end
+
+-- Lib = {
+--     foo = function (x, y) return x+y end 
+--     goo = function (x, y) return x-y end 
+-- }
+
 function Lib.foo (x, y) return x+y end
 function Lib.goo (x, y) return x-y end
 
