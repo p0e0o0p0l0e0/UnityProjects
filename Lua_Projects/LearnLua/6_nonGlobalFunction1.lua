e2698d39e5f347255c784e0d753d1cd156030dd1
CYOU-INC\zhanglinqi <p0e0o0p0l0e0@sina.com>
2018-02-17 17:14:56 +0800

    6 闭合函数（重新定义预定义的函数）

--- Lua_Projects/LearnLua/6_nonGlobalFunction1.lua
+++ Lua_Projects/LearnLua/6_nonGlobalFunction1.lua
@@ -1,11 +1,17 @@
 -- local fact = function (n)
 --     if n == 0 then return 1
+--     else return n * fact(n-1) -- fact不存在，因为还没有定义结束，下面的两种方式都ok
+--     end
+-- end
+
+-- local fact
+-- fact = function (n)
+--     if n == 0 then return 1
 --     else return n * fact(n-1)
 --     end
 -- end
 
-local fact
-fact = function (n)
+local function fact (n)
     if n == 0 then return 1
     else return n * fact(n-1)
     end
