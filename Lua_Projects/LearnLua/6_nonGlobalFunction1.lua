cdbc2be48ab16760504ac7d7e7f3ebebb8cdd199
CYOU-INC\zhanglinqi <p0e0o0p0l0e0@sina.com>
2018-02-17 17:00:43 +0800

    6 局部递归函数的定义，需要先声明

---
+++ Lua_Projects/LearnLua/6_nonGlobalFunction1.lua
@@ -0,0 +1,14 @@
+-- local fact = function (n)
+--     if n == 0 then return 1
+--     else return n * fact(n-1)
+--     end
+-- end
+
+local fact
+fact = function (n)
+    if n == 0 then return 1
+    else return n * fact(n-1)
+    end
+end
+
+print (fact(4))
